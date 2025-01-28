112451dc2eb2f8e4d5a78d9f78e95a45b538d9cc
Alex Sørlie <alex@sorlie.io>
2025-01-28 21:05:48 +0000

    nvim: Remove mouse in hardtime, block vV
    Signed-off-by: Alex Sørlie <alex@sorlie.io>

--- nvim/.config/nvim/lua/alex/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/alex/plugins/hardtime.lua
@@ -4,12 +4,19 @@ return {
         "m4xshen/hardtime.nvim",
         dependencies = { "MunifTanjim/nui.nvim" },
         opts = {
-            disable_mouse = false,
             disabled_keys = {
                 ["<Up>"] = {},
                 ["<Down>"] = {},
                 ["<Left>"] = {},
                 ["<Right>"] = {},
+            },
+            hints = {
+                ["v%V"] = {
+                    message = function()
+                        return "Use V instead of vV" -- return the hint message you want to display
+                    end,
+                    length = 2,                      -- the length of actual key strokes that matches this pattern
+                },
             },
         }
     },
