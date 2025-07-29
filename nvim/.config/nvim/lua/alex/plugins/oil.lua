6575601fb84ab2ea45a42aa9bb547a5b85c4e0d9
Alex Sørlie <alex@sorlie.io>
2025-07-29 09:50:21 +0200

    Update oil on fs changes
    Signed-off-by: Alex Sørlie <alex@sorlie.io>

--- nvim/.config/nvim/lua/alex/plugins/oil.lua
+++ nvim/.config/nvim/lua/alex/plugins/oil.lua
@@ -7,7 +7,8 @@ return {
                 deault_file_explorer = true,
                 view_options = {
                     show_hidden = true,
-                }
+                },
+                watch_for_changes = true,
             }
             vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         end,
