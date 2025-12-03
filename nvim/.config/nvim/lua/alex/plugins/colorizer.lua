4a5e2740ef17bd3d3baf723d6cdae5192e594f74
Alex Sørlie <alex@sorlie.io>
2025-12-04 00:24:37 +0100

    Enable colorizer for Rust with RGB/RGBA support

--- nvim/.config/nvim/lua/alex/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/alex/plugins/colorizer.lua
@@ -1,8 +1,20 @@
 return {
     {
         "norcalli/nvim-colorizer.lua",
-        config = function ()
-            require 'colorizer'.setup()
+        config = function()
+            require 'colorizer'.setup({
+                'rust',
+                'css',
+                'html',
+                'javascript',
+                'typescript',
+            }, {
+                RGB = true,
+                RRGGBB = true,
+                RRGGBBAA = true,
+                rgb_fn = true,   -- CSS rgb() and rgba()
+                hsl_fn = true,   -- CSS hsl() and hsla()
+            })
         end
     }
 }
