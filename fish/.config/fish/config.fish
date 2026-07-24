a692f2876a832e086f483caf67cb4628a5d62a4d
Alex Sørlie <alex@sorlie.io>
2026-07-24 21:03:42 +0200

    chore(fish): add opencode bin to PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -59,3 +59,6 @@ alias ta='tmux attach'
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# opencode
+fish_add_path /Users/alex/.opencode/bin
